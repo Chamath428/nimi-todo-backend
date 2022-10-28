867b34c5d6ef0d4b99e188b2e8d5853bf896d5ad
Chamath Chinthana <62209498+Chamath428@users.noreply.github.com>
2022-10-28 07:46:07 +0530

    after creating add-to-do

--- controllers/todo-controller.js
+++ controllers/todo-controller.js
@@ -1,1 +1,16 @@
 import express from 'express';
+import { addToDoModel } from '../models/todo-model.js';
+import connection from '../database.js';
+
+export const addToDo = async(req,res)=>{
+    const query=`INSERT INTO todo (activity,description,date,start_time,end_time,status) VALUES ("${req.body.activity}","${req.body.description}","${req.body.date}","${req.body.start_time}","${req.body.end_time}",${req.body.status})`;
+
+    connection.query(query,(err,rows,fields)=>{
+        if(err){
+            res.status(400).send(err)
+        }else{
+            res.status(200).send({"id":rows.insertId});
+        }
+    });
+}
+
--- routes/todo-routers.js
+++ routes/todo-routers.js
@@ -1,3 +1,8 @@
 import express from 'express';
+import { addToDo,getAllToDo,getToDoById,deleteToDo,updateToDo } from '../controllers/todo-controller.js';
 
 const todoRouters = express.Router();
+
+todoRouters.post('/add',addToDo);
+
+export  default todoRouters;
